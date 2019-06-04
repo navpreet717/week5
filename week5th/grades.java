3d2407b2ed29c3677b4d4114c3d6d111e4daa68e
748674 <748674@123-PC17.student.Tor.Loc>
2019-06-04 15:29:50 -0400

    grades input

---
+++ week5th/grades.java
@@ -0,0 +1,48 @@
+package week5th;
+
+import java.util.Scanner;
+
+public class grades {
+
+	public static void main(String[] args) {
+		
+		
+	}
+	//function ask user to enter 5 test scores
+		//convert score to letter grade
+		//return letter as string
+		//a:90-100
+		//b:80-89
+	//c:70-79
+	//d:60-69
+	//f:below 60
+	
+	
+		
+		public static String getLetterScore(){
+			//ASK 5 GRADES MEANS INPUT
+			//SCANNER
+			Scanner keyboard=new Scanner(System.in);
+			
+		for(int i=0;i<5;i++){
+			System.out.println("Enter Scores"+i);
+			int score=keyboard.nextInt();
+			
+		}
+
+			
+			
+			
+			//CALCULATE AVERAGE
+			
+			//DECIDE WHAT LETTER PERSON GETS
+			
+			//IF ELSE
+			
+			
+			//RETURN LETTER GRADES
+		}
+		return ;
+
+	}
+
