73f3c0d276470d14ec35653f407363a086d0514f
748674 <748674@123-PC17.student.Tor.Loc>
2019-06-04 14:17:31 -0400

    //function without return type

--- week5th/Calculation_function.java
+++ week5th/Calculation_function.java
@@ -3,16 +3,24 @@ package week5th;
 public class Calculation_function {
 
 	public static void main(String[] args) {
+		calculateTax(100.00);
+		
+	}
+	
 		// TODO Auto-generated method stub
 
 		// TODO Auto-generated method stub
-//function:a small program that runs inside a bigger program
-		//properties of function
-		//you can reuse the function
-		//function have name --- all function have some name
-		//function have parameters-optional
-		//function have a return type
-		//you put your statements inside the function
+//calculate tax u pay
+		//take one parameter price
+		// output total tax on the price
+		//output to console then void
+		
+		
+			public static void calculateTax (double price){
+				final double taxrate=0.13;
+				double result=price*taxrate;
+				System.out.println("tax is:"+result);
+		
 	}
 
 }
