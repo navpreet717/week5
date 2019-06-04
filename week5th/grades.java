127deabf99c849340d06aa7adaa1c33c887a4e8f
748674 <748674@123-PC17.student.Tor.Loc>
2019-06-04 15:45:02 -0400

    grades output

--- week5th/grades.java
+++ week5th/grades.java
@@ -7,6 +7,11 @@ public class grades {
 	public static void main(String[] args) {
 		
 		
+		
+		String finalGrade=getLetterScore();
+		System.out.println("final scores"+finalGrade);
+		
+		
 	}
 	//function ask user to enter 5 test scores
 		//convert score to letter grade
@@ -23,26 +28,50 @@ public class grades {
 			//ASK 5 GRADES MEANS INPUT
 			//SCANNER
 			Scanner keyboard=new Scanner(System.in);
-			
+			int score=0;
 		for(int i=0;i<5;i++){
-			System.out.println("Enter Scores"+i);
-			int score=keyboard.nextInt();
+			System.out.println("Enter Scores"+(i+1));
+			score=score+keyboard.nextInt();
 			
 		}
 
-			
+		System.out.println("total scores"+score);
 			
 			
 			//CALCULATE AVERAGE
+			double average=score/5.0;
+			System.out.println("average scores"+average);
 			
 			//DECIDE WHAT LETTER PERSON GETS
+			String LetterScore="";
+			if(average>=90 && average<=100)
+			{
+				 LetterScore="A";
+			}
+			else if(average>=80 && average<=89)
+			{
+				 LetterScore="B";
+			}
+			else if(average>=70 && average<=79)
+			{
+				 LetterScore="C";
+			}
+			else if(average>=60 && average<=69)
+			{
+				 LetterScore="D";
+			}
+			else 
+			{
+				 LetterScore="E";
+			}
 			
 			//IF ELSE
 			
 			
 			//RETURN LETTER GRADES
-		}
-		return ;
+			
+		return LetterScore;
+}
 
 	}
 
