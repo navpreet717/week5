4c17cb5d7b3f4f049fa7addd3f884e915aed3bf2
748674 <748674@123-PC17.student.Tor.Loc>
2019-06-04 14:29:36 -0400

    //flippy diostance traveled

---
+++ week5th/Flippy.java
@@ -0,0 +1,25 @@
+package week5th;
+
+public class Flippy {
+
+	public static void main(String[] args) {
+		 calculateDistance(9.8,10);//call the function
+		
+	}
+		// TODO Auto-generated method stub
+		//the distance an object falls down
+		//d=0.5*g*t ----->g gravity(9.8m/s^2), t time--> how long its falling 
+		//two parameters
+		//p1 gravity
+		// p2 time in seconds
+		// function should output in console
+		
+		public static void calculateDistance(double g,int t){
+			double distance=0.5*g*t;
+			System.out.println("distance travelled:"+distance);
+			
+		}
+		
+
+	}
+
