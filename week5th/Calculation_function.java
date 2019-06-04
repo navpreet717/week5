27ab4fd332356e9e0232042da935fdbcb8569407
748674 <748674@123-PC17.student.Tor.Loc>
2019-06-04 13:35:58 -0400

    initial commit

---
+++ week5th/Calculation_function.java
@@ -0,0 +1,18 @@
+package week5th;
+
+public class Calculation_function {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// TODO Auto-generated method stub
+//function:a small program that runs inside a bigger program
+		//properties of function
+		//you can reuse the function
+		//function have name --- all function have some name
+		//function have parameters-optional
+		//function have a return type
+		//you put your statements inside the function
+	}
+
+}
